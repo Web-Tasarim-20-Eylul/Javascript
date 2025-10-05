0ebf98de5f59c9fd706741fd43bc92367893ba50
bedirhan-1 <bedirhan9828@gmail.com>
2025-10-05 14:45:01 +0300

    if else örnekler

--- if-else/kullanim.js
+++ if-else/kullanim.js
@@ -33,16 +33,37 @@
 
 // console.log(3);
 
-let ayseninYasi = 43;
-let ayseninKilosu = 56;
-let ayseninBoyu = 166;
+// let ayseninYasi = 43;
+// let ayseninKilosu = 56;
+// let ayseninBoyu = 166;
 
-if (ayseninYasi > 18) {
-  if (ayseninBoyu < 170) {
-    if (ayseninKilosu <= 60) {
-      console.log(1);
-    }
-    console.log(2);
-  }
-  console.log(3);
+// if (ayseninYasi > 18) {
+//   if (ayseninBoyu < 170) {
+//     if (ayseninKilosu <= 60) {
+//       console.log(1);
+//     }
+//     console.log(2);
+//   }
+//   console.log(3);
+// }
+
+// const temperature = 25;
+// const isRaining = false;
+
+// if (temperature > 20 && !isRaining) {
+//   console.log("Hava dışarı çıkmak için güzel");
+// } else {
+//   console.log("Evde kalmak daha iyi");
+// }
+
+const a = 10;
+const b = 5;
+const c = "10";
+
+if (a == c && a > b) {
+  console.log("1");
+} else if (a === c || b < 3) {
+  console.log("2");
+} else {
+  console.log("3");
 }
