80a120006435c0f03d1e92b87556fd126198b14c
bedirhan-1 <bedirhan9828@gmail.com>
2025-10-12 14:49:46 +0300

    ornekler

--- flow/ornek4.js
+++ flow/ornek4.js
@@ -1,3 +1,15 @@
-//
+let arr = [40, 72, 67, 59, 93, 8000];
 
-let arr = [40, 72, 67, 59, 93];
+// (40 + 72 + 67 + 59 + 93) / 5;
+
+var toplam = 0;
+var gecerliSayiMiktari = 0;
+
+for (let sayac = 0; sayac < arr.length; sayac++) {
+  if (0 <= arr[sayac] && arr[sayac] <= 100) {
+    toplam += arr[sayac];
+    gecerliSayiMiktari++;
+  }
+}
+
+console.log(toplam / gecerliSayiMiktari);
